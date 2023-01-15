0e2103a3b7342ded3b9e47bf35441f6ad75be786
MEAAD OUDAH ALSERAIHI <1612602@stu.kau.edu.sa>
2023-01-16 00:14:49 +0300

    changes

---
+++ udagram/set_env.sh
@@ -0,0 +1,16 @@
+# This file is used for convenience of local development.
+# This file is used for convenience of local development.
+# DO NOT STORE YOUR CREDENTIALS INTO GIT
+export POSTGRES_USERNAME = postgres
+export POSTGRES_PASSWORD = postgres
+export POSTGRES_HOST = database-1.cgxil2r5lihq.us-east-1.rds.amazonaws.com
+export POSTGRES_DB = postgres
+export DB_PORT = 5432
+export PORT = 3000
+export AWS_BUCKET = arn:aws:s3:::meaad-bucket
+export AWS_REGION = us-east-1
+export AWS_PROFILE = default
+export JWT_SECRET = mysecretstring
+export AWS_ACCESS_KEY_ID = "AKIAUMDB64OZQFEZZDF7"
+export AWS_SECRET_ACCESS_KEY = "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"
+export URL = http://udagram-api-dev.eba-p3ssepcr.us-east-1.elasticbeanstalk.com/
